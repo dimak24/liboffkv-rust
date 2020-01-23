514f60a7d52692b4dafba3979ca73fa12826d664
Dmitriy <dimak24.05@mail.ru>
2020-01-24 00:41:52 +0300

    .travis.yml: fixed linux build

--- build.rs
+++ build.rs
@@ -1,14 +1,18 @@
 extern crate cmake;
 
 fn main() {
-    // TODO: add option to build without installing liboffkv from submodule
+    // TODO
+    let mut toolchain = std::env::home_dir().unwrap();
+    toolchain.push("vcpkg/scripts/buildsystems/vcpkg.cmake");
 
+    // TODO: add option to build without installing liboffkv from submodule
     let mut dst = cmake::Config::new("liboffkv")
         .define("BUILD_TESTS", "OFF")
         .define("ENABLE_ZK", "OFF")
         .define("ENABLE_CONSUL", "ON")
         .define("ENABLE_ETCD", "OFF")
         .define("BUILD_CLIB", "ON")
+        .define("CMAKE_TOOLCHAIN_FILE", toolchain.as_os_str())
         .build_target("")
         .build();
 
---
+++ travis/bootstrap_consul.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+TRIPLET="$1"_amd64
+EXT=zip
+curl -L https://releases.hashicorp.com/consul/1.5.1/consul_1.5.1_${TRIPLET}.${EXT} -o $HOME/consul.${EXT}
+
+mkdir -p $HOME/consul
+unzip -q $HOME/consul.${EXT} -d $HOME/consul
+
+rm -f $HOME/consul.${EXT}
+cd $HOME/consul
+
+./consul agent -dev > /dev/null &
